57b3d1d5b61711c070cfc722b89be2339b720d37
student <student@10.2.3.1>
2016-05-23 14:57:22 +0200

    JUnit testy

--- pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
+++ pl.edu.us.awpto/src/test/java/kolokwium/a/pl/edu/us/awpto/FizzBuzzTest.java
@@ -2,8 +2,50 @@ package kolokwium.a.pl.edu.us.awpto;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class FizzBuzzTest
 {
+	private int parameter;
+	private String expected; 
+	
+	public FizzBuzzTest(int testParameter, String expected) {
+		 this.parameter = testParameter;
+		 this.expected = expected;
+		 }
+	
+	@Parameters
+	public static Collection<Object[]> none()
+	{
+		Object[][] none = new Object[][] { {1, ""+1}, {2, ""+2}, {4, ""+4}, {5, "Buzz"}, {9, "Fizz"}, {15, "FizzBuzz"}, {60, "FizzBuzz"}  };
+		return Arrays.asList(none);
+	}
+	@Test
+	public void test() {
+		
+		FizzBuzz obj = new FizzBuzz();
+		
+		assertEquals("Test", expected, obj.calculate(parameter));
+		/*
+		if(parameter % 3 == 0 && parameter % 5 == 0)
+			assertEquals("Test", "FizzBuzz", obj.calculate(parameter));
+		else if(parameter % 3 == 0)
+			assertEquals("Test 3", "Fizz", obj.calculate(parameter));
+		else if(parameter % 5 == 0)
+			assertEquals("Test 3", "Buzz", obj.calculate(parameter));
+		else
+			assertEquals("Test 3", ""+parameter, obj.calculate(parameter));
+		*/
+		
+	}
+	
+	
+	
 }
